1964ba06bb00036ded37d38fe48ebbdb44f3dca4
Bastien Dumont <bastienxs@gmail.com>
2023-03-19 21:43:05 +0100

    hide controls when game is finished

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -10,18 +10,20 @@ export const Game = ({ list, actor }: { list: Result[]; actor: Result }) => {
   const [filteredList, setFilteredList] = useState(list);
 
   const [guesses, addGuess] = useState<string[]>([]);
-  const [success, setSuccess] = useState(false);
+  const [success, setSuccess] = useState<boolean>(false);
   const [userChoice, setUserChoice] = useState<string>();
+  const [end, setEnd] = useState<boolean>(false);
 
   useEffect(() => {
-    if (userInput !== '') {
-      const newList = filteredList.filter((actor) =>
-        actor.name.toLowerCase().includes(userInput)
-      );
-      setFilteredList(newList);
-    } else {
+    if (userInput === '') {
       setFilteredList(list);
+      return;
     }
+
+    const newList = filteredList.filter((actor) =>
+      actor.name.toLowerCase().includes(userInput)
+    );
+    setFilteredList(newList);
   }, [userInput]);
 
   const submitChoice = () => {
@@ -35,33 +37,49 @@ export const Game = ({ list, actor }: { list: Result[]; actor: Result }) => {
     addGuess((oldState) => [...oldState, userChoice]);
     if (userChoice === actor.id.toString()) {
       setSuccess(true);
+      setEnd(true);
     }
   };
+
+  useEffect(() => {
+    if (guesses.length >= 5) {
+      setEnd(true);
+    }
+  }, [guesses]);
 
   return (
     <>
       <div>Tries : {guesses.length + 1} / 6</div>
-      <input
-        type='text'
-        value={userInput}
-        onChange={(e) => setUserInput(e.target.value.toLowerCase())}
-      ></input>
-      <div className='listContainer'>
-        {filteredList.map((actor) => (
-          <div
-            key={actor.id}
-            onClick={() => setUserChoice(actor.id.toString())}
-          >
-            {actor.name}
+      {!end && (
+        <>
+          <input
+            type='text'
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value.toLowerCase())}
+          />
+          <div className='listContainer'>
+            {filteredList.map((actor) => (
+              <div
+                key={actor.id}
+                onClick={() => setUserChoice(actor.id.toString())}
+                className={userChoice == actor.id.toString() ? 'active' : ''}
+                style={
+                  userChoice == actor.id.toString()
+                    ? { backgroundColor: 'red' }
+                    : {}
+                }
+              >
+                {actor.name}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      )}
       <button type='button' onClick={submitChoice}>
         send result
       </button>
-      {success && 'gagné'}
-      {guesses.length >= 5 && success && <h2>You win !</h2>}
-      {guesses.length >= 5 && !success && <h2>You loose :(</h2>}
+      {success && <h2>You win !</h2>}
+      {end && !success && <h2>You loose :(</h2>}
     </>
   );
 };
