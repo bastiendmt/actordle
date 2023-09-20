ddb6300ae795311c617c97da8983d5c089505bbc
Bastien Dumont <bastienxs@gmail.com>
2023-09-20 08:47:06 +0200

    app router stable

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const { withSentryConfig } = require('@sentry/nextjs');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     remotePatterns: [
       {
